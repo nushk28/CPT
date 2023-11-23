62fcb45b2fc06a6b50ddc09a2ffbfd2fd7dd3d5c
Anushka Srivastava <nushk.28@gmail.com>
2023-11-24 00:30:59 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/', Router);
+app.use(express.static('./client/build'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 8000;
 
